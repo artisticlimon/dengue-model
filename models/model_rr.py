6020d706c17daa2e28a22503d6222cd396c111e9
artisticlimon <m.penawust.pd@gmail.com>
2026-06-05 09:29:44 -0600

    fix invalid threshold parameter and use random state for gridsearch

--- models/model_rr.py
+++ models/model_rr.py
@@ -377,18 +377,18 @@ class Model:
                     estimator=best_clf,
                     scoring="roc_auc",
                     cv=5,
-                    n_jobs=-1
+                    n_jobs=-1,
+                    random_state=42
                 )
                 clf_tuned.fit(X_combined, y_train_bin)
                 y_pred_classi = clf_tuned.predict(X_test)
-                print(f"Optimal threshold: {clf_tuned.threshold_}")
                 grid_classi = clf_tuned  
             except ValueError as e:
                 print(f"Threshold tuning failed: {e}")
                 print("Using best classifier directly without threshold tuning.")
                 y_pred_classi = best_clf.predict(X_test)
                 grid_classi = best_clf  
-                
+
             print(classification_report(y_test_bin, y_pred_classi))
             # RocCurveDisplay.from_predictions(y_test_bin, clf.predict_proba(X_test)[:, 1], plot_chance_level= True)
 
@@ -410,11 +410,11 @@ class Model:
                     estimator=best_clf,
                     scoring="roc_auc",
                     cv=5,
-                    n_jobs=-1
+                    n_jobs=-1,
+                    random_state=42
                 )
                 clf_tuned.fit(X_combined, y_train_bin)
                 y_pred_classi = clf_tuned.predict(X_test)
-                print(f"Optimal threshold: {clf_tuned.threshold_}")
                 grid_classi = clf_tuned  
             except ValueError as e:
                 print(f"Threshold tuning failed: {e}")
